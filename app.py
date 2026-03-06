42bcd17526220e98378b6735047faadf86daca2c
yuval99399-eng <yuval99399@gmail.com>
2026-03-06 10:47:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ with tab_credit:
                     avg_comp = m_comp.groupby('ענף')['סכום חיוב'].mean().reset_index()
                     avg_comp['Month-Year'] = 'Average (Selected Period)' 
                     combined_comp = pd.concat([m_comp, avg_comp], ignore_index=True)
-                    fig_bar = px.bar(m_comp, x='ענף', y='סכום חיוב', color='Month-Year', barmode='group', text='סכום חיוב',color_discrete_sequence=px.colors.qualitative.Prism)
+                    fig_bar = px.bar(combined_comp, x='ענף', y='סכום חיוב', color='Month-Year', barmode='group', text='סכום חיוב',color_discrete_sequence=px.colors.qualitative.Prism)
                     fig_bar.update_traces(texttemplate='%{text:.2s}', textposition='outside', textangle=-90)
                     fig_bar.update_layout(yaxis_range=[0, m_comp['סכום חיוב'].max() * 1.25], margin=dict(t=50))
                     st.plotly_chart(fig_bar, use_container_width=True)
